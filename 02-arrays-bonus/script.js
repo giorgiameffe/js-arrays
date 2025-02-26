4f2e5d8a908ebe681b581b830f6eeec5a477c31b
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-26 21:26:52 +0100

    feat: add exercise 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,18 +8,28 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+ console.log(teachers);
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+const reversedTeachers = teachers.length - 1;
+
+for (let i = reversedTeachers; i >= 0; i--) {
+  console.log(teachers[i]);
+}
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
