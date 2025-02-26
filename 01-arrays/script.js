d22a2c0bd00327962e45355769da2462ca10d135
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-26 17:52:28 +0100

    feat: add exercises 1-2-3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,15 +8,25 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+console.log(teachers);
+
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+
+const fourthTeacher = teachers[4];
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
+teachers[4] = 'Patrick';
+console.log(teachers);
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+const lastTeacher = teachers.pop();
+console.log(teachers);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
@@ -25,10 +35,12 @@ const firstTeacher = null;
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
-// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers 
 // e salvalo nella variabile lewisIndex
 const lewisIndex = null;
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = null;
+
+
